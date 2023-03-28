762b7a533fbfe640efbf3db67e7677c7ceed3064
Yukti Manoj Mulani <105308291+YuktiMulani@users.noreply.github.com>
2023-03-28 19:58:25 -0400

    MAINTAING

---
+++ Project2/shoppinglist.h
@@ -0,0 +1,16 @@
+#ifndef SDDS_SHOPPINGLIST_H
+#define SDDS_SHOPPINGLIST_H
+namespace sdds {
+	const int MAX_NO_OF_RECS = 15;
+	bool loadList();
+	void displayList();
+	void removeBoughtItems();
+	void removeItem(int index);
+	bool saveList();
+	void clearList();
+	void toggleBought();
+	void addItemToList();
+	void removeItemfromList();
+	bool listIsEmpty();
+}
+#endif
